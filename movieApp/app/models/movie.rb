9fb841f4fe633b854ff9c91589592e36b51e1f9f
Mitchell Moore <mmoorejr24@gmail.com>
2019-11-25 12:27:47 -0800

    image resizing functionality to work.

--- movieApp/app/models/movie.rb
+++ movieApp/app/models/movie.rb
@@ -1,3 +1,16 @@
-class Movie < ApplicationRecord
+require 'uploads'
+class Movie < ApplicationRecord	
 	belongs_to :user
+	has_one_attached :image
+
+	validate :image_format
+
+	private
+
+	def image_format
+		return unless image.attached?
+		return if image.blob.content_type
+		image.purge_later
+		errors.add(:image, 'Needs to be an image')
+	end
 end
