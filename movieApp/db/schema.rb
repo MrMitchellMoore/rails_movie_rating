f44edfb6c9779c350a166a8276e6086b81db47dd
Mitchell Moore <mmoorejr24@gmail.com>
2019-11-25 17:33:39 -0800

    made reviews model and add user to reviews

--- movieApp/db/schema.rb
+++ movieApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_175451) do
+ActiveRecord::Schema.define(version: 2019_11_26_013208) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2019_11_25_175451) do
     t.string "movie_length"
     t.string "director"
     t.string "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
